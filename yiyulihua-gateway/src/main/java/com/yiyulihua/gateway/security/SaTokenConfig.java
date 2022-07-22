6ff5743aab958afd835f2250e422beb6ed4ea6f1
志昊的刘 <1138423425@qq.com>
2022-07-22 17:54:07 +0800

    feat:关闭认证授权

--- yiyulihua-gateway/src/main/java/com/yiyulihua/gateway/security/SaTokenConfig.java
+++ yiyulihua-gateway/src/main/java/com/yiyulihua/gateway/security/SaTokenConfig.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
-@Configuration
+//@Configuration
 public class SaTokenConfig {
 
     private static final String[] EXCLUDE_PATH_PATTERNS = {
--- yiyulihua-gateway/src/main/java/com/yiyulihua/gateway/security/StpInterfaceImpl.java
+++ yiyulihua-gateway/src/main/java/com/yiyulihua/gateway/security/StpInterfaceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
+//@Component
 public class StpInterfaceImpl implements StpInterface {
 
     @Override
